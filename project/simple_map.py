236f334454ff644cec24ab015caccd6c784e1587
Aimee Barciauskas <abarciauskas@mdsol.com>
2016-03-30 19:05:11 +0200

    Fix viterbi for directions

--- project/simple_map.py
+++ project/simple_map.py
@@ -1,18 +1,19 @@
 import itertools
+import time 
 execfile('optimum_policy.py')
+
 grid = [[0,0],
-        [0,0]]
+        [1,0]]
 
 rows = range(len(grid))
 cols = range(len(grid[0]))
 # delta = orientation
 states = list(itertools.product(rows,cols,range(len(delta))))
-print(states)
-init = states[0]
-goal = states[-1]
-
+init = states[0][0:2]
+goal = states[-1][0:2]
+print(init)
 # determine optimal path
-opt = optimum_policy(grid, goal, cost)
+opt = optimum_policy(grid, init, cost)
 for i in range(0,len(opt)):
   print(opt[i])
 
@@ -25,8 +26,10 @@ for i in range(0,len(opt)):
 # generate the transition matrix
 # for every state in states
 trans_p = {}
+emit_p = {}
 for state in states:
   trans_p[state] = {}
+  emit_p[state] = {}
   x = state[0]
   y = state[1]
   o = state[2]
@@ -34,23 +37,42 @@ for state in states:
   current_value = opt[x][y]
   # for every delta in deltas
   for d in range(len(delta)):
+    # should only check this possibility if we can move there
     x2 = x + delta[d][0]
     y2 = y + delta[d][1]
     if not x2 == -1 and not y2 == -1 and x2 < len(opt) and y2 < len(opt[1]):
       # check the gain of that transition
       gain = opt[x2][y2] - current_value
-      # if gain is positive and in the same direction as orientation, give high probability
+      # if gain is positive and in the same direction as current orientation, give high probability
       if gain == 1 and delta[o] == delta[d]:
-        trans_p[state][(x2,y2,d)] = 0.9
+        trans_p[state][(x2,y2,d)] = 0.8
+        emit_p[state][d] = 0.8
       elif gain > 90:
         trans_p[state][(x2,y2,d)] = 0.0
+        emit_p[state][d] = 0.0
       # if looking in opposite direction
       elif gain == 1 and delta[o] == delta[d-2]:
-        trans_p[state][(x2,y2,d)] = 0.0
+        trans_p[state][(x2,y2,d)] = 0.2
+        emit_p[state][d] = 0.2
+      elif (x2,y2) == goal:
+        print('gooooal')
+        trans_p[state][(x2,y2,d)] = 0.8
+        emit_p[state][d] = 0.8
       else:
         trans_p[state][(x2,y2,d)] = 0.2
+        emit_p[state][d] = 0.2
+  # but we need to have something for everybody
+  # what states are we missing?
+  missing_state_transitions = set(states).difference(set(trans_p[state].keys()))
+  for missing_state in missing_state_transitions:
+    trans_p[state][missing_state] = 0.0
+  missing_deltas = set(range(len(delta))).difference(emit_p[state].keys())
+  for missing_delta in missing_deltas:
+    emit_p[state][missing_delta] = 0.0
 
-print trans_p
+# print trans_p
+# print('')
+# print emit_p
       # if orientation is in direction of highest gain, weight that probablitiy very high
 #   if orientation is in opposite direction of highest gain, weight that probability 0
 #   weight other directions corresponding to gain of that move
--- project/viterbi.py
+++ project/viterbi.py
@@ -1,37 +1,6 @@
-# States correspond to positions of Xavier
-# TODO: Add orientations?
-# State space S
-states = ((0,0),(0,1),(1,1),(1,0))
-# e.g. directives motivated by current belief state
-# ADD MOVE UP
-# Observation space O
-obs = ('move left', 'move down', 'stay put', 'stay put')
-# given the origin is 1 and the goal is 4, prefer to move left (when oriented down)
-# chosen somewhat arbitrarily
-# Transition matrix A
-trans = {
-    (0,0): {(0,0): 0.1, (0,1): 0.8, (1,1): 0.0, (1,0): 0.1},
-    (0,1): {(0,0): 0.1, (0,1): 0.1, (1,1): 0.8, (1,0): 0.0},
-    (1,1): {(0,0): 0.0, (0,1): 0.1, (1,1): 0.8, (1,0): 0.0},
-    (1,0): {(0,0): 0.1, (0,1): 0.0, (1,1): 0.8, (1,0): 0.1}
-}
-# Probability of getting directive
-# ADD MOVE UP
-# Emission matrix B
-emiss = {
-    (0,0): {'move left': 0.7, 'move right': 0.1, 'move down': 0.1, 'stay put': 0.1},
-    (0,1): {'move left': 0.1, 'move right': 0.1, 'move down': 0.7, 'stay put': 0.1},
-    (1,1): {'move left': 0.1, 'move right': 0.1, 'move down': 0.1, 'stay put': 0.7},
-    (1,0): {'move left': 0.7, 'move right': 0.1, 'move down': 0.1, 'stay put': 0.1}
-}
-# An initial array of probabilities of size K - e.g. the possible number of states
-# Here, say it's known as the first state
-# DATA STRUCTURES KNOWLEDGE: Tuples have structure, lists have order
-# http://stackoverflow.com/questions/626759/whats-the-difference-between-list-and-tuples
-piarr = (0.9,0.05,0.05,0)
+execfile('simple_map.py')
 
-
-def viterbi(states, piarr, trans, emiss, obs):
+def viterbi(states, piarr, trans_p, emit_p, obs):
   # Initialize T1 and T2, which keep track of everything done so far
   # T1 - probability of most likely path so far
   T1 = [{}]
@@ -41,20 +10,23 @@ def viterbi(states, piarr, trans, emiss, obs):
   for s in range(0, len(states)):
     st = states[s]
     # s is 1-4, so subtract 1 for indexing into piarr
-    T1[0][s] = piarr[s]*emiss[st][obs[0]]
+    if obs[0] in emit_p[st].keys():
+      T1[0][s] = piarr[s]*emit_p[st][obs[0]]
+    else:
+      T1[0][s] = 0.0
   for t in range(1, T):
     T1.append({})
     for s in range(0, len(states)):
       st = states[s]
       # evaluate the probabilitiy of each possible state transition
       # on the basis of the transition probability and the current observation
-      prob_each_step = [T1[(t-1)][y0]*trans[states[y0]][st]*emiss[st][obs[t]] for y0 in range(0,len(states))]
+      prob_each_step = [T1[(t-1)][y0]*trans_p[states[y0]][st]*emit_p[st][obs[t]] for y0 in range(0,len(states))]
       maxprob = max(prob_each_step)
       argmaxk = prob_each_step.index(maxprob)
       T1[t][s] = maxprob
   for i in dptable(T1):
       print i
-  # terminal state
+  #terminal state
   opt = []
   for j in T1:
     for x, y in j.items():
@@ -70,5 +42,12 @@ def dptable(V):
     for y in V[0]:
         yield "%.7s: " % y+" ".join("%.7s" % ("%f" % v[y]) for v in V)
 
-print(viterbi(states, piarr, trans, emiss, obs))
+piarr = [0.05]*len(states)
+piarr[2] = 0.9
+# grid = [[0,1],
+#         [0,0]]
+# last observation has to make sense for position
+# obs = (2,3,1)
+obs = (3,2,0)
+print(viterbi(states, piarr, trans_p, emit_p, obs))
 
